8476f27ae986f9e2ea8991ba8861d1c09b85a083
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-13 16:23:18 -0300

    Reinderizando todos

--- JavaScript/modulo03/todos.js
+++ JavaScript/modulo03/todos.js
@@ -1,1 +1,21 @@
-alert('funcionou');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector('#app buttom');
+
+var todos = [
+  'Fazer café',
+  'Estudar JavaScript',
+  'Acessar comunidade da Rocketseat',
+];
+
+function renderTodos() {
+  for (todo of todos) {
+    var todoElement = document.createElement('li');
+    var todoText = document.createTextNode(todo);
+
+    todoElement.appendChild(todoText);
+    listElement.appendChild(todoElement);
+  }
+}
+
+renderTodos();
