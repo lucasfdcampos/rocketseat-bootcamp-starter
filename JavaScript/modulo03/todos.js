fd5cef691542f003b2232a223ebfecd4afa9b526
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-20 22:18:46 -0300

    Salvando no Storage

--- JavaScript/modulo03/todos.js
+++ JavaScript/modulo03/todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-  'Fazer café',
-  'Estudar JavaScript',
-  'Acessar comunidade da Rocketseat',
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
   listElement.innerHTML = '';
@@ -44,6 +40,7 @@ function addTodo() {
   inputElement.focus();
 
   renderTodos();
+  saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -51,4 +48,9 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
   todos.splice(pos, 1);
   renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('list_todos', JSON.stringify(todos));
 }
