9af95f1cb9b00ebd8600cce7da8e4110af9fcb95
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-21 20:45:01 -0300

    Promises

--- JavaScript/modulo04/main.js
+++ JavaScript/modulo04/main.js
@@ -1,3 +1,4 @@
+/*
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://api.github.com/users/lucasfdcampos');
@@ -8,3 +9,30 @@ xhr.onreadystatechange = function () {
     console.log(JSON.parse(xhr.responseText));
   }
 };
+*/
+
+var minhaPromise = function () {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users/lucasfdcampos');
+    xhr.send(null);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject('Erro na requisição');
+        }
+      }
+    };
+  });
+};
+
+minhaPromise()
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.warn(error);
+  });
