c924eb59cd49e80b08aed0c48767738a5cde49dd
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-20 22:11:00 -0300

    Exclui todos

--- JavaScript/modulo03/todos.js
+++ JavaScript/modulo03/todos.js
@@ -1,5 +1,5 @@
 var listElement = document.querySelector('#app ul');
-var inputElement = document.querySelector("#app input");
+var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
 var todos = [
@@ -15,7 +15,21 @@ function renderTodos() {
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement('a');
+    linkElement.setAttribute('href', '#');
+
+    var pos = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.appendChild(linkText);
+
+    // adiciona o texto no li
     todoElement.appendChild(todoText);
+    // adiciona link para excluir
+    todoElement.appendChild(linkElement);
+    // adiciona o li (todo) no ul
     listElement.appendChild(todoElement);
   }
 }
@@ -33,3 +47,8 @@ function addTodo() {
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+  todos.splice(pos, 1);
+  renderTodos();
+}
