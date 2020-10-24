8fa0e879337be8f7477c1c7a7d116cf80d3acbcb
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-24 17:00:53 -0300

    Operacoes em Array - reduce e filter

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -1,7 +1,15 @@
 "use strict";
 
 var arr = [1, 3, 4, 5, 8, 9];
-var newArr = arr.map(function (item) {
-  return item * 2;
+var newArr = arr.map(function (item, index) {
+  return item + index;
 });
 console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
