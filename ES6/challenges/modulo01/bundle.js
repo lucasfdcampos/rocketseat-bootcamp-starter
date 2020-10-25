3c3bade4a5e6d8d3fabf17a7aa60170b5e76bd75
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-25 15:39:35 -0300

    modulo 01 - exercicio 03

--- ES6/challenges/modulo01/bundle.js
+++ ES6/challenges/modulo01/bundle.js
@@ -1,53 +1,56 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+// 3.1
+var arr = [1, 2, 3, 4, 5];
+/*
+arr.map(function (item) {
+  return item + 10;
+});
+*/
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+var newArr = arr.map(function (item) {
+  return item + 10;
+});
+console.log(newArr); // 3.2
+// Dica: Utilize uma constante pra function
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var usuarios = [{
+var usuario = {
   nome: 'Diego',
-  idade: 23,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Gabriel',
-  idade: 15,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Lucas',
-  idade: 30,
-  empresa: 'Facebook'
-}]; // 2.1 Utilizando o map
-// Crie uma variável que contenha todas idades dos usuários: [23, 15, 30]
+  idade: 23
+};
 
-var idades = usuarios.map(function (usuario) {
+var mostraIdade = function mostraIdade(usuario) {
   return usuario.idade;
-});
-console.log(idades); // 2.2 Utilizando o filter
-// Crie uma variável que tenha apenas os usuários que trabalham na Rocketseat
-// e com mais de 18
-// anos: [{ nome: 'Diego', idade: 23, empresa: 'Rocketseat' }]
+};
 
-var usersRocketseat = usuarios.filter(function (usuario) {
-  return usuario.empresa === 'Rocketseat' && usuario.idade > 18;
-});
-console.log(usersRocketseat); // 2.3 Utilizando o find
-// Crie uma variável que procura por um usuário que trabalhe na empresa
-// Google: undefined
+console.log(mostraIdade(usuario)); // 3.3
+// Dica: Utilize uma constante pra function
 
-var usersGoogle = usuarios.find(function (usuario) {
-  return usuario.empresa === 'Google';
-});
-console.log(usersGoogle); // 2.4 Unindo operações
-// Multiplique a idade de todos usuários por dois e depois realize um
-// filtro nos usuários que possuem no máximo 50 anos
+var nome = 'Diego';
+var idade = 23;
 
-var usersSenior = usuarios.map(function (users) {
-  return _objectSpread(_objectSpread({}, users), {}, {
-    idade: users.idade * 2
+var mostraUsuario = function mostraUsuario() {
+  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Diego';
+  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
+  return {
+    nome: nome,
+    idade: idade
+  };
+};
+
+console.log(mostraUsuario(nome, idade));
+console.log(mostraUsuario(nome)); // 3.4
+
+/*
+const promise = function () {
+  return new Promise(function (resolve, reject) {
+    return resolve();
   });
-}).filter(function (users) {
-  return users.idade <= 50;
-});
-console.log(usersSenior);
+};
+*/
+
+var promise = function promise() {
+  return new Promise(resolve, function (reject) {
+    return resolve();
+  });
+};
