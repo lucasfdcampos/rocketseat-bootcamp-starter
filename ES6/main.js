e7735fca3b8c50878e00b47f4f2dec16f7bd9f6d
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-25 07:58:10 -0300

    Desestruturacao

--- ES6/main.js
+++ ES6/main.js
@@ -1,4 +1,22 @@
-const soma = (a = 3, b = 6) => a + b;
+const usuario = {
+  nome: 'Lucas',
+  idade: 29,
+  endereco: {
+    cidade: 'Arapongas',
+    estado: 'PR',
+  },
+};
 
-console.log(soma(1));
-console.log(soma());
+const {
+  nome,
+  idade,
+  endereco: { cidade },
+} = usuario;
+
+console.log(nome, idade, cidade);
+
+function mostraNome({ nome, idade }) {
+  console.log(nome, idade);
+}
+
+mostraNome(usuario);
