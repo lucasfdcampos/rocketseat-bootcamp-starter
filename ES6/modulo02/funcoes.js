74272def0fe65d7f0d7ba1ba02a96a961dfb833e
Lucas Campos <lucascampos.ads@gmail.com>
2020-10-27 22:03:31 -0300

    Import e Export

--- ES6/modulo02/funcoes.js
+++ ES6/modulo02/funcoes.js
@@ -1,3 +1,13 @@
+import { util } from 'webpack';
+
 export function soma(a, b) {
   return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export function mult(a, b) {
+  return a * b;
 }
--- ES6/modulo02/main.js
+++ ES6/modulo02/main.js
@@ -1,3 +1,6 @@
-import { soma } from './funcoes';
+import * as funcoes from './funcoes';
+//import somaFunction from './soma';
 
-console.log(soma(1, 2));
+console.log(funcoes.soma(5, 2));
+console.log(funcoes.sub(4, 2));
+console.log(funcoes.mult(3, 5));
