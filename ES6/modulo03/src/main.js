c0c73efd1f8e26267f75bb242bc1d508932df25b
Lucas Campos <lucascampos.ads@gmail.com>
2020-11-10 22:07:08 -0300

    modulo 03 - Arrow function async

--- ES6/modulo03/src/main.js
+++ ES6/modulo03/src/main.js
@@ -1,14 +1,17 @@
-const minhaPromise = () =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('OK');
-    }, 2000);
-  });
+import axios from 'axios';
 
-async function executaPromise() {
-  const response = await minhaPromise();
+class Api {
+  static async getUserInfo(username) {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${username}`
+      );
 
-  console.log(response);
+      console.log(response);
+    } catch (err) {
+      console.warn('Erro na API');
+    }    
+  }
 }
 
-executaPromise();
+Api.getUserInfo('lucasfdcampos');
