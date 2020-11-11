2cea28a387818928a95425f60afb8caa2cde9ad5
Lucas Campos <lucascampos.ads@gmail.com>
2020-11-10 21:47:25 -0300

    modulo 03 - Async/await

--- ES6/modulo03/src/main.js
+++ ES6/modulo03/src/main.js
@@ -1,1 +1,14 @@
-alert('testando - modulo03');
+const minhaPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('OK');
+    }, 2000);
+  });
+
+async function executaPromise() {
+  const response = await minhaPromise();
+
+  console.log(response);
+}
+
+executaPromise();
