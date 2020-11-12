95074dcd49e37cdcb8369d06538942760c8241f5
Lucas Campos <lucascampos.ads@gmail.com>
2020-11-11 21:28:11 -0300

    modulo 03 - exercicio 01 1.1

--- ES6/challenges/modulo03/src/main.js
+++ ES6/challenges/modulo03/src/main.js
@@ -1,1 +1,12 @@
-alert('funcionando')
+import axios from 'axios';
+
+// 1.1
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+const umPorSegundo = async () => {
+  await delay(console.log('1s'));
+  await delay(console.log('2s'));
+  await delay(console.log('3s'));
+}
+
+umPorSegundo();
